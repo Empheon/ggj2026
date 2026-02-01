6ecc63dbd66ecca517b5b5f513169abc45fffc74
François de la Taste <canardbleu@gmail.com>
2026-02-01 13:47:39 +0100

    juice title menu

--- scenes/title_menu.gd
+++ scenes/title_menu.gd
@@ -1,20 +1,12 @@
 extends Control
 
-@export var title_1 : Label
-@export var title_2 : Label
-@export var play_button : Button
+@export var play_button : BaseButton
 
 func _ready() -> void:
-	title_1.self_modulate.a = 0.0
-	title_2.self_modulate.a = 0.0
 	play_button.hide()
 	await get_tree().create_timer(0.4).timeout
 	var tween := create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_ELASTIC)
 	tween.set_parallel()
-	tween.tween_property(title_1,"scale",Vector2.ONE,1.5).from(Vector2.ONE*0.2)
-	tween.tween_property(title_1,"self_modulate:a",1.0,0.4)
-	tween.tween_property(title_2,"scale",Vector2.ONE,1.5).from(Vector2.ONE*0.2).set_delay(0.5)
-	tween.tween_property(title_2,"self_modulate:a",1.0,0.4).set_delay(0.5)
 	tween.tween_callback(play_button.show).set_delay(1)
 	tween.tween_property(play_button,"scale",Vector2.ONE,0.6).from(Vector2.ONE*0.6).set_delay(1)
 	tween.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUART)\
@@ -22,5 +14,5 @@ func _ready() -> void:
 		.from(Color(4,4,4,1)).set_delay(1)
 	await tween.finished
 	var loop_tween := create_tween().set_loops()
-	loop_tween.tween_property(play_button,"self_modulate",Color(2,2,2,1),1)
+	loop_tween.tween_property(play_button,"self_modulate",Color(1.3,1.3,1.3,1),1)
 	loop_tween.tween_property(play_button,"self_modulate",Color(1,1,1,1),1)
