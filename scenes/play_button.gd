7b36c2c9af84d215adb58ef2c2a2ea0e75bed486
François de la Taste <canardbleu@gmail.com>
2026-02-01 14:12:15 +0100

    integration UI boutons

--- scenes/play_button.gd
+++ scenes/play_button.gd
@@ -4,4 +4,6 @@ func _ready() -> void:
 	connect("pressed", _loadMainScene)
 
 func _loadMainScene() -> void:
+	self_modulate = Color.WEB_GRAY
+	await get_tree().process_frame
 	get_tree().change_scene_to_file('res://scenes/main_scene.tscn')
